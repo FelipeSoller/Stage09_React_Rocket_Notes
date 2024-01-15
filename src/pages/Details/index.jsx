d1141b31ce640e153243a98025c952a9da710097
Felipe Soller <felipe.souza@imaginedone.com.br>
2024-01-15 11:22:15 -0300

    add links section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,25 @@
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 import { Header } from '../Header'
-import { Container } from './styles'
+import { Container, Links } from './styles'
 export const Details = () => {
   return (
     <Container>
       <Header></Header>
-      <Button title='Voltar' />
+      <Section title='Useful links'>
+        <Links>
+          <li>
+            <a href='#'>Link 1</a>
+          </li>
+          <li>
+            <a href='#'>Link 2</a>
+          </li>
+          <li>
+            <a href='#'>Link 3</a>
+          </li>
+        </Links>
+      </Section>
+      <Button title='Back' />
     </Container>
   )
 }
