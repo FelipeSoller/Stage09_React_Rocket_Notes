25c21a597c2dbe793b11ccdaec541f2e14e21817
Felipe Soller <soller@outlook.com>
2024-01-15 12:06:35 -0300

    finishing details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,32 +5,45 @@ import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
+
 export const Details = () => {
   return (
     <Container>
       <Header />
-      <ButtonText title={'Delete note'}/>
-      <Section title='Useful links'>
-        <Links>
-          <li>
-            <a href='#'>Link 1</a>
-          </li>
-          <li>
-            <a href='#'>Link 2</a>
-          </li>
-          <li>
-            <a href='#'>Link 3</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title={'Delete note'}/>
 
-      <Section title='Tags'>
-        <Tag title='Node.JS' />
-        <Tag title='Express' />
-        <Tag title='JavaScript' />
-      </Section>
-      <Button title='Back' />
+          <h1>
+            React Fundamentals
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos cupiditate molestiae enim. Alias error aut distinctio fugiat, ab illo nostrum accusantium, saepe quos mollitia fuga, perspiciatis porro debitis animi voluptates.
+          </p>
+          <Section title='Useful links'>
+            <Links>
+              <li>
+                <a href='#'>Link 1</a>
+              </li>
+              <li>
+                <a href='#'>Link 2</a>
+              </li>
+              <li>
+                <a href='#'>Link 3</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title='Tags'>
+            <Tag title='Node.JS' />
+            <Tag title='Express' />
+            <Tag title='JavaScript' />
+          </Section>
+          <Button title='Back' />
+        </Content>
+      </main>
     </Container>
   )
 }
