e944e34bf8661c9d9579de27262be00b9ae4b84d
Felipe Soller <soller@outlook.com>
2024-01-15 16:59:06 -0300

    create Sign In page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,37 @@
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Background, Container, Form } from './styles'
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>React Rocket Notes</h1>
+        <p>Applicatioon to write and save your notes</p>
+
+        <h2>Sign In</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Password"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Enter" />
+
+        <a href="#">Sign Up</a>
+      </Form>
+
+      <Background />
+    </Container>
+  )
+}
