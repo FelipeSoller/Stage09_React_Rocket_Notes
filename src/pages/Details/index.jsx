ab8c13b8f54beda1a9ed75970d23889e0b16189a
Felipe Soller <soller@outlook.com>
2024-01-15 15:55:13 -0300

    replace Header component to the correct fold

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Header } from '../Header'
+import { Header } from '../../components/Header'
 
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
