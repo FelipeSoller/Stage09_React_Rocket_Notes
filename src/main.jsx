20f34cb084a176b25736bc9b7999db73e4d994e6
Felipe Soller <soller@outlook.com>
2024-01-14 22:55:39 -0300

    create Details page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+
+import { Details } from './pages/Details'
+
 import GlobalStyle from './styles/global'
 import theme from './styles/theme'
 
@@ -8,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from '../../components/Button'
+import { Container } from './styles'
+export const Details = () => {
+  return (
+    <Container>
+      <h1>Details</h1>
+      <Button />
+    </Container>
+  )
+}
