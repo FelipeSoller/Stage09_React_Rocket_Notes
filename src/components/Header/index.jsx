d8b19182d201f9f4db676c19023c0f1d2bfb86d7
Felipe Soller <soller@outlook.com>
2024-02-27 13:53:17 -0300

    add redirect to initial page when logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,9 +6,16 @@ import { api } from "../../service/api"
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 import { Container, Profile, Logout } from "./styles"
+import { useNavigate } from "react-router-dom"
 
 export const Header = () => {
   const { signOut, user } = useAuth()
+  const navigation = useNavigate()
+
+  const handleSignOut = () => {
+    navigation('/')
+    signOut()
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
@@ -27,7 +34,7 @@ export const Header = () => {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
